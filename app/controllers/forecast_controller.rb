0c50ef82caec181e2414b79f336db257219079d0
howabunga <weihowa@gmail.com>
2015-07-21 01:05:10 -0500

    after Monday
    2 of 3 complete

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,22 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+url = "https://api.forecast.io/forecast/19d48afa13d3f3921cb021d54c4c0ce5/#{@lat},#{@lng}"
 
+# APIKEY = "19d48afa13d3f3921cb021d54c4c0ce5"
 
-    @current_temperature = "Replace this string with your answer."
+raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
 
-    @current_summary = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
